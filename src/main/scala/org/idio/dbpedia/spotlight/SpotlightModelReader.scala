0ea3c76ab26f9bc426ff644f41243abf7c01008a
David Przybilla <dav.alejandro@gmail.com>
2014-01-10 14:40:29 +0000

    refactoring command line interface

--- src/main/scala/org/idio/dbpedia/spotlight/SpotlightModelReader.scala
+++ src/main/scala/org/idio/dbpedia/spotlight/SpotlightModelReader.scala
@@ -27,102 +27,109 @@ object Main{
       println("reading models...")
       val spotlightModelReader =  Main.getSpotlightModel(pathToModelFolder)
 
+      action match{
 
-      if (action.equals("show-resource-types")){
-        val typeStore = spotlightModelReader.idioDbpediaResourceStore.resStore.ontologyTypeStore.asInstanceOf[MemoryOntologyTypeStore]
-        for(ontologyType<-typeStore.idFromName.keySet().toArray){
-          println(ontologyType)
+        // prints all types in type store
+        case "show-resource-types" =>{
+          val typeStore = spotlightModelReader.idioDbpediaResourceStore.resStore.ontologyTypeStore.asInstanceOf[MemoryOntologyTypeStore]
+          for(ontologyType<-typeStore.idFromName.keySet().toArray){
+            println(ontologyType)
+          }
+        }
+
+        // get the statistics for a surface form
+        case "check" =>{
+          val surfaceText = args(2)
+          println("getting statistics for surfaceText.....")
+          spotlightModelReader.getStatsForSurfaceForm(surfaceText)
+        }
+
+        //show context words
+        case "show-context" =>{
+          val dbpediaURI = args(2)
+          spotlightModelReader.prettyPrintContext(dbpediaURI)
+        }
+
+        // checks whether a dbpedia URI exists or not
+        case "search" =>{
+          val dbpediaURI = args(2)
+          println("getting statistics for surfaceText.....")
+          val searchResult:Boolean = spotlightModelReader.searchForDBpediaResource(dbpediaURI)
+          if (searchResult){
+            println(dbpediaURI+" exists")
+          }else{
+            println(dbpediaURI+" NOT FOUND")
+          }
+        }
+
+        //attach surface form and topic, if they dont exist they are created
+        case "update" =>{
+          val surfaceForm = args(2)
+          val dbpediaURI = args(3)
+
+          var types = Array[String]()
+          if (args.length ==5)
+            types = args(4).split("|")
+
+          println("addding new sf and concept")
+          val contextWords = Array[String]()
+          val contextCounts = Array[Int]()
+          spotlightModelReader.addNew(surfaceForm,dbpediaURI,types,contextWords,contextCounts)
+          println("getting the stats for the new surfaceForm")
+          spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
+
+          //exporting for testing purpouses
+          spotlightModelReader.exportModels("/Users/dav009/IdeaProjects/untitled/out/artifacts/untitled_jar")
+        }
+
+
+        //boost the values for a surfaceForm and a topic
+        case "boost" =>{
+          val surfaceForm = args(2)
+          val dbpediaURI = args(3)
+          val boostValue = args(4).toInt
+
+          println("statistics before the boost...")
+          spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
+
+          spotlightModelReader.boostValue(surfaceForm, dbpediaURI, boostValue)
+
+          println("statistics after the boost..")
+          spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
+        }
+
+        // outputs the properties for 40 Surface forms.
+        case "explore" =>{
+          spotlightModelReader.showSomeSurfaceForms()
         }
       }
 
-      // get the statistics for a surface form
-      if (action.equals("check")){
-        val surfaceText = args(2)
-        println("getting statistics for surfaceText.....")
-        spotlightModelReader.getStatsForSurfaceForm(surfaceText)
-      }
-
-      //show context words
-      if (action.equals("show-context")){
-        val dbpediaURI = args(2)
-        spotlightModelReader.prettyPrintContext(dbpediaURI)
-      }
-
-      // checks whether a dbpedia URI exists or not
-      if (action.equals("search")){
-        val dbpediaURI = args(2)
-        println("getting statistics for surfaceText.....")
-        val searchResult:Boolean = spotlightModelReader.searchForDBpediaResource(dbpediaURI)
-        if (searchResult){
-          println(dbpediaURI+" exists")
-        }else{
-          println(dbpediaURI+" NOT FOUND")
-        }
-
-      }
-
-      //attach surface form and topic, if they dont exist they are created
-      if (action.equals("update")){
-        val surfaceForm = args(2)
-        val dbpediaURI = args(3)
-
-        var types = Array[String]()
-        if (args.length ==5)
-          types = args(4).split("|")
-
-        println("addding new sf and concept")
-        val contextWords = Array[String]()
-        val contextCounts = Array[Int]()
-        spotlightModelReader.addNew(surfaceForm,dbpediaURI,types,contextWords,contextCounts)
-        println("getting the stats for the new surfaceForm")
-        spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
-
-        //exporting for testing purpouses
-        spotlightModelReader.exportModels("/Users/dav009/IdeaProjects/untitled/out/artifacts/untitled_jar")
-      }
-
-
-      //boost the values for a surfaceForm and a topic
-      if (action.equals("boost")){
-        val surfaceForm = args(2)
-        val dbpediaURI = args(3)
-        val boostValue = args(4).toInt
-
-        println("statistics before the boost...")
-        spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
-
-        spotlightModelReader.boostValue(surfaceForm, dbpediaURI, boostValue)
-
-        println("statistics after the boost..")
-        spotlightModelReader.getStatsForSurfaceForm(surfaceForm)
-
-      }
-
-      // outputs the properties for 40 Surface forms.
-      if (action.equals("explore")){
-        spotlightModelReader.showSomeSurfaceForms()
-      }
     }else{
-      // update model from file
-      if (action.equals("file-update-sf-dbpedia")){
-        val pathToFileWithAdditions = args(2)
-        val modelUpdater:ModelUpdateFromFile = new ModelUpdateFromFile(pathToModelFolder, pathToFileWithAdditions)
-        modelUpdater.loadNewEntriesFromFile()
-      }
 
-      // update context words from file
-      if (action.equals("file-update-context")){
-        val pathToFileWithAdditions = args(2)
-        val modelUpdater:ContextUpdateFromFile = new ContextUpdateFromFile(pathToModelFolder, pathToFileWithAdditions)
-        modelUpdater.loadContextWords()
-      }
+        action match{
 
-      //checks existence of Dbpedia's Ids, SF, and links between SF's and Dbpedia's ids.
-      if (action.equals("file-check")){
-        val pathToFileWithResources = args(2)
-        val modelExplorer:ModelExplorerFromFile = new ModelExplorerFromFile(pathToModelFolder, pathToFileWithResources)
-        modelExplorer.checkEntitiesInFile()
-      }
+              // update model from file
+              case "file-update-sf-dbpedia" => {
+                val pathToFileWithAdditions = args(2)
+                val modelUpdater:ModelUpdateFromFile = new ModelUpdateFromFile(pathToModelFolder, pathToFileWithAdditions)
+                modelUpdater.loadNewEntriesFromFile()
+              }
+
+              // update context words from file
+              case "file-update-context" =>{
+                val pathToFileWithAdditions = args(2)
+                val modelUpdater:ContextUpdateFromFile = new ContextUpdateFromFile(pathToModelFolder, pathToFileWithAdditions)
+                modelUpdater.loadContextWords()
+              }
+
+              //checks existence of Dbpedia's Ids, SF, and links between SF's and Dbpedia's ids.
+              case "file-check" =>{
+                val pathToFileWithResources = args(2)
+                val modelExplorer:ModelExplorerFromFile = new ModelExplorerFromFile(pathToModelFolder, pathToFileWithResources)
+                modelExplorer.checkEntitiesInFile()
+              }
+
+        }
 
     }
 
